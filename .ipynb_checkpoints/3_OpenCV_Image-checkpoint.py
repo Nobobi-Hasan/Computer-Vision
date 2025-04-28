e38e7fcaee3fe0e737c1b5b5aeb12228762dbd9b
Nobobi-Hasan <shatin.hasan73@gmail.com>
2025-04-29 00:04:33 +0600

    Drawing on Image

--- .ipynb_checkpoints/3_OpenCV_Image-checkpoint.py
+++ .ipynb_checkpoints/3_OpenCV_Image-checkpoint.py
@@ -6,7 +6,7 @@ while True:
     cv2.imshow("puppy",img)
     
     # If we've waited at least 1ms AND pressed the Esc
-    if cv2.waitKey(1) & 0xFF == 27:
+    if cv2.waitKey(0) & 0xFF == 27:
         break
         
 cv2.destroyAllWindows()
